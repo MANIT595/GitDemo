7e5ede4309090616e83fe262508e1867966ecabd
manikanta <manit595@gmail.com>
2024-03-18 14:54:56 -0400

    user service added to git

---
+++ UserService/src/test/java/com/manikanta/microservice/project/UserService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manikanta.microservice.project.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
